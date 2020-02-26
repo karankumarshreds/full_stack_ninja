439002a45918ee436a5736e91a7aea7b8bfc47f8
Karan Kumar <nailedITnerd@gmail.com>
2020-02-26 20:58:04 +0530

    list created

--- articles/models.py
+++ articles/models.py
@@ -5,3 +5,6 @@ class Articles(models.Model):
     slug = models.SlugField()
     body = models.TextField()
     date = models.DateTimeField(auto_now_add=True)
+    
+    def __str__(self):
+        return self.title
--- articles/views.py
+++ articles/views.py
@@ -1,6 +1,11 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Articles
 
 # Create your views here.
-def articles(request):
-    return render(request, 'articles/articles_list.html', {})
+def articles_list(request):
+    articles = Articles.objects.all()
+    context = {
+        'articles': articles,
+    }
+    return render(request, 'articles/articles_list.html', context)
