abafc45f2bfc0ac88a53f801c766ac8e810045bd
Karan Kumar <nailedITnerd@gmail.com>
2020-03-02 01:59:11 +0530

    added login view

--- accounts/views.py
+++ accounts/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render, redirect
 from django.http import HttpResponse
 from django.contrib.auth.forms import UserCreationForm as uc_form
+from django.contrib.auth.forms import AuthenticationForm as a_form
+from django.contrib.auth import login
 
 # Create your views here.
 def register(request):
@@ -8,11 +10,27 @@ def register(request):
 	if request.method == 'POST':
 		form = uc_form(request.POST)
 		if form.is_valid():
-			form.save()
+			user = form.save()
+			login(request, user)
 			return redirect('/articles')
 	else:		
 		form = uc_form()
-		context = {
-			'form': form
-		}
+	context = {'form': form}
 	return render(request, 'accounts/register.html', context)
+
+def log_in(request):
+
+	if request.method == 'POST':
+		form = a_form(data=request.POST)
+		if form.is_valid():
+			user = form.get_user()
+			login(request, user)
+			return redirect('/articles')
+		else:
+			return redirect('/accountslogin')
+	else:
+		form = a_form()
+		context = {'form': form}
+		return render(request, 'accounts/login.html', context)
+
+
